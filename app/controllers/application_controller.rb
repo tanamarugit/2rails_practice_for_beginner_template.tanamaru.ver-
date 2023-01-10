783e5a14744f78ffd991992f578939f3abcf70d1
tanamaru <yt.0408.mmm@gmail.com>
2023-01-10 20:32:58 +0900

    ログイン、ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  before_action :login_required
   add_flash_types :success, :info, :warning, :danger
+  
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    redirect_to login_path unless current_user
+  end
 end
---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,29 @@
+class SessionController < ApplicationController
+  skip_before_action :login_required
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: session_params[:email])
+    
+    if @user &.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      redirect_to users_path, notice: 'ログインしました。'
+    else
+      render :login
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to login_path, notice: 'ログアウトしました。'
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
